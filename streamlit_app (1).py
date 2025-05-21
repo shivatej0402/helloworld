99fa370bfda470c8e8f2bf53fa4dccca59cd6637
shivatej0402 <tejshiva422@gmail.com>
2025-05-21 12:10:45 +0530

    Update streamlit_app (1).py

--- streamlit_app (1).py
+++ streamlit_app (1).py
@@ -2,3 +2,6 @@ import streamlit as st
 
 st.title("👋 Hello, World!")
 st.write("This is my first Streamlit app running inside Snowflake!")
+
+st.title("🌟 Updated App")
+st.write("This version reflects recent changes!")
